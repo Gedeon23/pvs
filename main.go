699b183be441bf4852c1ae4453f1e79a52036bf7
gedeon <47500169+Gedeon23@users.noreply.github.com>
2024-09-14 15:44:10 +0200

    ADD interface for celestial bodies and better scaling

--- main.go
+++ main.go
@@ -15,43 +15,65 @@ const (
 	AU float32 = 1000.0
 
 	// Megameter
-	GM float32 = 1.0 / 150.0
+	MM float32 = 1.0 / 150_000.0
+
+	BODY_SCALE float32 = 10_000.0
 )
 
 type Body interface {
 	Draw()
 }
 
-type CelestialBody struct {
+type CelestialBody interface {
+	Draw()
+}
+
+type Star struct {
 	Position rl.Vector3
 	Radius   float32
 	Color    color.RGBA
 }
 
-func (a CelestialBody) Draw() {
-	rl.DrawSphere(a.Position, a.Radius, a.Color)
+func (s Star) Draw() {
+	rl.DrawSphere(s.Position, s.Radius, s.Color)
 }
 
-var Earth CelestialBody = CelestialBody{
+type Planet struct {
+	Position rl.Vector3
+	Radius   float32
+	Color    color.RGBA
+}
+
+func (p Planet) Draw() {
+	rl.DrawSphere(p.Position, p.Radius, p.Color)
+}
+
+type Moon struct {
+	Position rl.Vector3
+	Radius   float32
+	Color    color.RGBA
+}
+
+func (m Moon) Draw() {
+	rl.DrawSphere(m.Position, m.Radius, m.Color)
+}
+
+var Earth Planet = Planet{
 	Position: rl.Vector3{X: AU, Y: 0, Z: 0},
-	Radius:   6.371 * GM,
+	Radius:   6.371 * MM * BODY_SCALE,
 	Color:    color.RGBA{121, 110, 211, 255},
 }
 
-var Moon CelestialBody = CelestialBody{
-	Position: rl.Vector3{X: Earth.Position.X, Y: 0, Z: Earth.Radius + 400*GM},
-	Radius:   1.737 * GM,
+var TheMoon Moon = Moon{
+	Position: rl.Vector3{X: Earth.Position.X, Y: 0, Z: Earth.Radius + 400*MM*BODY_SCALE},
+	Radius:   1.737 * MM * BODY_SCALE,
 	Color:    color.RGBA{111, 111, 111, 255},
 }
 
-var Sun CelestialBody = CelestialBody{
+var Sun Star = Star{
 	Position: rl.Vector3{X: 0, Y: 0, Z: 0},
-	Radius:   696.340 * GM,
+	Radius:   696.340 * MM * BODY_SCALE,
 	Color:    color.RGBA{211, 181, 111, 255},
-}
-
-func DrawCelestialBody(body CelestialBody) {
-	rl.DrawSphere(body.Position, body.Radius, body.Color)
 }
 
 func main() {
@@ -63,8 +85,8 @@ func main() {
 	rl.DisableCursor()
 
 	camera := rl.Camera3D{
-		Position:   Moon.Position,
-		Target:     Earth.Position,
+		Position:   Earth.Position,
+		Target:     Sun.Position,
 		Up:         rl.Vector3{X: 0, Y: 1, Z: 0},
 		Fovy:       45,
 		Projection: rl.CameraPerspective,
@@ -78,12 +100,14 @@ func main() {
 		rl.DrawText("fps: "+strconv.FormatInt(int64(rl.GetFPS()), 10), 10, 10, 20, rl.LightGray)
 
 		rl.BeginMode3D(camera)
-		DrawCelestialBody(Earth)
-		DrawCelestialBody(Sun)
-		DrawCelestialBody(Moon)
+		if rl.Vector3DotProduct(Sun.Position, rl.Vector3Normalize(camera.Target)) > 0.95 {
+			rl.DrawText("You are looking at the sun", 10, 35, 20, rl.LightGray)
+		}
+		TheMoon.Draw()
+		Sun.Draw()
+		Earth.Draw()
 		rl.EndMode3D()
 		rl.EndDrawing()
 	}
-
-	fmt.Printf("Moon coords: %f %f %f, rad: %f", Moon.Position.X, Moon.Position.Y, Moon.Position.Z, Moon.Radius)
+	fmt.Printf("%f", rl.Vector3DotProduct(Sun.Position, rl.Vector3Normalize(camera.Target)))
 }
