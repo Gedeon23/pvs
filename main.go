6dc4166d261dcfc62b90ea3bcf9ac7e98dd10e7f
gedeon <47500169+Gedeon23@users.noreply.github.com>
2024-09-15 11:13:29 +0200

    ADD display for currently viewed celestial body

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"strconv"
 
@@ -26,9 +25,12 @@ type Body interface {
 
 type CelestialBody interface {
 	Draw()
+	GetPosition() rl.Vector3
+	GetName() string
 }
 
 type Star struct {
+	Name     string
 	Position rl.Vector3
 	Radius   float32
 	Color    color.RGBA
@@ -38,7 +40,16 @@ func (s Star) Draw() {
 	rl.DrawSphere(s.Position, s.Radius, s.Color)
 }
 
+func (s Star) GetPosition() rl.Vector3 {
+	return s.Position
+}
+
+func (s Star) GetName() string {
+	return s.Name
+}
+
 type Planet struct {
+	Name     string
 	Position rl.Vector3
 	Radius   float32
 	Color    color.RGBA
@@ -48,7 +59,16 @@ func (p Planet) Draw() {
 	rl.DrawSphere(p.Position, p.Radius, p.Color)
 }
 
+func (p Planet) GetPosition() rl.Vector3 {
+	return p.Position
+}
+
+func (p Planet) GetName() string {
+	return p.Name
+}
+
 type Moon struct {
+	Name     string
 	Position rl.Vector3
 	Radius   float32
 	Color    color.RGBA
@@ -58,19 +78,30 @@ func (m Moon) Draw() {
 	rl.DrawSphere(m.Position, m.Radius, m.Color)
 }
 
+func (m Moon) GetPosition() rl.Vector3 {
+	return m.Position
+}
+
+func (m Moon) GetName() string {
+	return m.Name
+}
+
 var Earth Planet = Planet{
+	Name:     "Earth",
 	Position: rl.Vector3{X: AU, Y: 0, Z: 0},
 	Radius:   6.371 * MM * BODY_SCALE,
 	Color:    color.RGBA{121, 110, 211, 255},
 }
 
 var TheMoon Moon = Moon{
+	Name:     "Moon",
 	Position: rl.Vector3{X: Earth.Position.X, Y: 0, Z: Earth.Radius + 400*MM*BODY_SCALE},
 	Radius:   1.737 * MM * BODY_SCALE,
 	Color:    color.RGBA{111, 111, 111, 255},
 }
 
 var Sun Star = Star{
+	Name:     "Sun",
 	Position: rl.Vector3{X: 0, Y: 0, Z: 0},
 	Radius:   696.340 * MM * BODY_SCALE,
 	Color:    color.RGBA{211, 181, 111, 255},
@@ -92,6 +123,12 @@ func main() {
 		Projection: rl.CameraPerspective,
 	}
 
+	celestBodies := []CelestialBody{
+		Sun,
+		Earth,
+		TheMoon,
+	}
+
 	for !rl.WindowShouldClose() {
 		rl.UpdateCamera(&camera, rl.CameraFree)
 		rl.BeginDrawing()
@@ -99,15 +136,20 @@ func main() {
 		rl.ClearBackground(rl.Black)
 		rl.DrawText("fps: "+strconv.FormatInt(int64(rl.GetFPS()), 10), 10, 10, 20, rl.LightGray)
 
-		rl.BeginMode3D(camera)
-		if rl.Vector3DotProduct(Sun.Position, rl.Vector3Normalize(camera.Target)) > 0.95 {
-			rl.DrawText("You are looking at the sun", 10, 35, 20, rl.LightGray)
+		writtenLines := 0
+		for _, body := range celestBodies {
+			sun_camera_proj := rl.Vector3DotProduct(rl.Vector3Subtract(body.GetPosition(), camera.Position), rl.Vector3Normalize(camera.Target))
+			if sun_camera_proj > 0.95 {
+				rl.DrawText("You are looking at the "+body.GetName(), 10, int32(35+writtenLines*25), 20, rl.LightGray)
+				writtenLines++
+			}
 		}
-		TheMoon.Draw()
-		Sun.Draw()
-		Earth.Draw()
+
+		rl.BeginMode3D(camera)
+		for _, body := range celestBodies {
+			body.Draw()
+		}
 		rl.EndMode3D()
 		rl.EndDrawing()
 	}
-	fmt.Printf("%f", rl.Vector3DotProduct(Sun.Position, rl.Vector3Normalize(camera.Target)))
 }
